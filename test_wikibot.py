6b3cbe9a5553c1aad08d4a822206bb04dfd7c3d0
LoicSteve <loikstevegmail.com>
2024-07-16 15:25:57 +0200

    linting

--- test_wikibot.py
+++ test_wikibot.py
@@ -3,14 +3,12 @@ from click.testing import CliRunner
 from wikibot import cli
 
 
-
-
-
 def test_wikibot():
-  runner = CliRunner()
-  result = runner.invoke(cli, ['--name', 'Microsoft'])
-  assert result.exit_code == 0
-  assert "Microsoft" in result.output 
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--name", "Microsoft"])
+    assert result.exit_code == 0
+    assert "Microsoft" in result.output
+
 
 def test_scrape():
     assert "Microsoft" in scrape("Microsoft")
--- wikibot.py
+++ wikibot.py
@@ -3,16 +3,13 @@ from mylib.bot import scrape
 
 import click
 
-@click.command()
-@click.option('--name',
-              help='The web page we wanna scrape.')
 
+@click.command()
+@click.option("--name", help="The web page we wanna scrape.")
 def cli(name):
     result = scrape(name)
-    click.echo(click.style(f"{result}", bg = 'red',fg='blue'))
+    click.echo(click.style(f"{result}", bg="red", fg="blue"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
-
-
-
